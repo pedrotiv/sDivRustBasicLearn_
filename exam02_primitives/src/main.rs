741dc838250b8260bca2964efd793427fbceeeb0
pedrotiv <pedrotiva@gmail.com>
2020-01-17 16:39:26 -0300

    fezt exam02: add new code in main()

--- exam02_primitives/src/main.rs
+++ exam02_primitives/src/main.rs
@@ -1,3 +1,8 @@
+// The following struct is for the activity.
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+
 fn main() {
                             // PRIMITIVES //
 
@@ -25,7 +30,7 @@ fn main() {
     // Variables can be overwritten with shadowing.
     let mutable = true;
 
-                            // LITERAL AND OPERATORS //
+                        // LITERAL AND OPERATORS //
 
         // Integer addition
         println!("1 + 2 = {}", 1u32 + 2);
@@ -52,12 +57,73 @@ fn main() {
 
 
                         // TUPLES //
+    // A tuple with a bunch of different types
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,-1i8,
+        -2i16, -3i32, -4i64,0.1f32, 0.2f64,'a', true);
 
+    // Values can be extracted from the tuple using tuple indexing
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    // Tuples can be tuple members
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    // Tuples are printable
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    // But long Tuples cannot be printed
+    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    // println!("too long tuple: {:?}", too_long_tuple);
+    // TODO ^ Uncomment the above 2 lines to see the compiler error
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+
+    //tuples can be destructured to create bindings
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+
+// WORK IN TI ****************************************
+    // #[derive(Debug)]
+    // let ti = (4,false);
+    // #[derive(Debug)]
+    // let tr = reverse(ti);
+    // println!("The reverse of the tuple {} is {}", ti,tr);
 
 }
 
+// Tuples can be used as function arguments and as return values
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    // `let` can be used to bind the members of a tuple to variables
+    let (integer, boolean) = pair;
 
-                    // NEXT: //
+    (boolean, integer)
+}
 
-// Make the anoted TODO tasks: ok, for now.
+
+                        // NEXT: //
+
+// Make the anoted TODO tasks:
 // Remember and anote bitwise operations
+
+                        // SOMES RUST BITWISE OPERATORS //
+                    
+// Bitwise AND or &	Retorna 1 em cada posição de bit para à qual o bit correspondente de ambos eram 1s.
+// Bitwise OR or |	Retorna 1 para cada posição de bit para à qual o correspondente de um ou ambos eram 1s.                    
+// Bitwise XOR or ^ Retorna 1 para cada posição de bit para à qual o bit correspondente de um mas não ambos eram 1s.
+// Bitwise NOT or ! Inverte os bits de seus operandos.
+// Left shift	a << b	Jogam a  em representação binária b (< 32) bits à esquerda, mudando de zeros à diretia.
+// Sign-propagating right shift	a >> b	Jogam a  em representação binária b (< 32) bits à direita, descartando bits que foram tornados off.
+
