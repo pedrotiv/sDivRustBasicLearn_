74aae69a47e471ce68d4a9ab157bce55ace4870c
pedrotiv <pedrotiva@gmail.com>
2020-01-24 16:34:06 -0300

    feat exam03: add constants.rs, end this chapter

--- exam03_custom_types/src/main.rs
+++ exam03_custom_types/src/main.rs
@@ -1,10 +1,12 @@
 // mod structures;
 // mod enums;
-mod enum_linked_list;
+// mod enum_linked_list;
+mod constants;
 
 
 fn main() {
     // structures::run();
     // enums::run();
-    enum_linked_list::run();
+    // enum_linked_list::run();
+    constants::run();
 }
