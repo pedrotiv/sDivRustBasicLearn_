a0cfa138f78922d74f8c830f1c5486cd5e7a0c73
pedrotiv <pedrotiva@gmail.com>
2020-02-14 11:35:55 -0300

    feat chap06: alter main.rs

--- chap06_enums_match/src/main.rs
+++ chap06_enums_match/src/main.rs
@@ -1,7 +1,6 @@
 // Enums allow you to define a type by enumerating its possible variants.
 // Rust’s enums are most similar to algebraic data types in functional languages
 
-
 //A Message enum whose variants each store different amounts and types of values
 #[derive(Debug)]
 enum Message {
@@ -11,7 +10,7 @@ enum Message {
     ChangeColor(i32, i32, i32),
 }
 //this is similar to defining different kinds of struct definitions for each variants
-// But if we used the different structs, which each have their own type, we couldn’t as 
+// But if we used the different structs, which each have their own type, we couldn’t as
 // easily define a function to take any of these kinds of messages
 impl Message {
     fn call(&self) {
@@ -21,5 +20,16 @@ impl Message {
 
 fn main() {
     let m = Message::Write(String::from("hello"));
-    println!("{:?}",m.call());
+    m.call();
+
+    // The Option<T> enum is so useful that it’s even included in the prelude; 
+    // you don’t need to bring it into scope explicitly. In addition, so are its variants:
+    //  you can use Some and None directly without the Option:: prefix. 
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+    println!("{:?}, {:?}, {:?}", some_number, some_string, absent_number);
+    println!("{:?}, {:?}, {:?}", some_number.unwrap(), some_string.unwrap(), absent_number);
+
+
 }
